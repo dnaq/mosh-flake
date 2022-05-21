e1e96a1397ccb991c37bd255eed366d5758c0714
dnaq <dnaq@users.noreply.github.com>
2022-05-21 16:39:58 +0200

    add overlay

--- flake.nix
+++ flake.nix
@@ -1,26 +1,35 @@
 {
   description = "tiny flake for hid_bootloader_cli";
   inputs = {
-      nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-      flake-utils.url = "github:numtide/flake-utils";
-      mosh = { url = "github:mobile-shell/mosh"; flake = false; };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mosh = {
+      url = "github:mobile-shell/mosh";
+      flake = false;
+    };
   };
-  
 
   outputs = { self, nixpkgs, flake-utils, ... }@attrs:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-          sources = { inherit (attrs) mosh; };
-      in
-      {
+      in {
         packages = rec {
-            mosh = import ./default.nix { inherit pkgs sources; };
-            default = mosh;
+          mosh = import ./default.nix {
+            inherit pkgs;
+            sources = attrs;
+          };
+          default = mosh;
         };
         apps.default = {
-            type = "app";
-            program = "${self.packages."${system}".mosh}/bin/mosh";
+          type = "app";
+          program = "${self.packages."${system}".mosh}/bin/mosh";
         };
-      }
-    );
+      }) // {
+        overlays.default = final: prev: {
+          mosh = import ./default.nix {
+            pkgs = prev;
+            sources = attrs;
+          };
+        };
+      };
 }
